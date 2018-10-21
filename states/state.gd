61279addb860f92d75ff7d31036c38414904698d
ekliot <ekliot@gmail.com>
2018-10-21 16:32:14 -0400

    document and touchup

--- states/state.gd
+++ states/state.gd
@@ -1,58 +1,96 @@
-# state.gd
+"""
+filename: state.gd
+"""
 
 extends Node
 
 signal state_enter
 signal state_leave
 
-var active = false
-var ID = '_'
+var ID = '_' setget ,get_state_id
+var active = false setget ,is_active
 
-func enter( fsm, last_state=null, state_data={} ):
-  print( '  state.gd // entering ', ID, ' from ', last_state )
-  _on_enter( fsm, last_state, state_data )
+func enter(fsm, last_state=null, state_data={}):
+  _on_enter(fsm, last_state, state_data)
   if not active:
-    emit_signal( 'state_enter' )
+    emit_signal('state_enter')
     active = true
 
-func leave( fsm ):
-  # print( '  state.gd // leaving ', ID )
-  _on_leave( fsm )
+func leave(fsm):
+  _on_leave(fsm)
   if active:
-    emit_signal( 'state_leave' )
+    emit_signal('state_leave')
     active = false
 
-# ============= #
-# OVERRIDEABLES #
-# ============= #
+"""
+=== OVERRIDEABLES
+    These are methods that extending classes are meant to override
+    Almost all of these are called by the StateMachine host
+"""
 
-func _on_enter( fsm, last_state=null, state_data={} ):
+func _on_enter(fsm, state_data={}, last_state=null):
+  """
+  Logic to execute when entering the state
+  Returns the state ID of the next state to change to, or null if no change needed
+  fsm :- the StateMachine host
+  state_data :- any relevant data this State may expect upon entering
+  last_state :- the last State the host was in
+  """
   return null
 
-func _on_leave( fsm ):
+func _on_leave(fsm):
+  """
+  Logic to execute when leaving the state
+  Returns the state ID of the next state to change to, or null if no change needed
+  fsm :- the StateMachine host
+  """
   return null
 
-func _update( fsm, delta ):
+func _update(fsm, delta):
+  """
+  A wrapper for Node._process(), called by the StateMachine host only when this State is active
+  Returns the state ID of the next state to change to, or null if no change needed
+  fsm :- the StateMachine host
+  delta :- delta time provided by Node._process()
+  """
   return null
 
-func _physics_update( fsm, delta ):
+func _physics_update(fsm, delta):
+  """
+  A wrapper for Node._physics_process(), called by the StateMachine host only when this State is active
+  Returns the state ID of the next state to change to, or null if no change needed
+  fsm :- the StateMachine host
+  delta :- delta time provided by Node._process()
+  """
   return null
 
-func _parse_input( fsm, ev ):
+func _parse_input(fsm, ev):
+  """
+  A wrapper for Node._input(), called by the StateMachine host only when this State is active
+  Returns the state ID of the next state to change to, or null if no change needed
+  fsm :- the StateMachine host
+  ev :- input event provided by Node._input()
+  """
   return null
 
-func _parse_unhandled_input( fsm, ev ):
+func _parse_unhandled_input(fsm, ev):
+  """
+  A wrapper for Node._unhandled_input(), called by the StateMachine host only when this State is active
+  Returns the state ID of the next state to change to, or null if no change needed
+  fsm :- the StateMachine host
+  ev :- input event provided by Node._unhandled_input()
+  """
   return null
 
-func _on_animation_finished( fsm, ani_name ):
+func _on_animation_finished(fsm, ani_name):
   return null
 
-# ====== #
-# GETSET #
-# ====== #
+"""
+=== GETTERS
+"""
 
-func get_id():
+func get_state_id():
   return ID
 
-func get_active():
+func is_active():
   return active
--- states/state_machine.gd
+++ states/state_machine.gd
@@ -1,99 +1,125 @@
-# state_machine.gd
+"""
+filename: state_machine.gd
+"""
 
 extends Node
 
-# emitted after the stack changes, and the previous state
-# is left, but before the next state is entered
-signal state_change # state_from, state_to
+# emitted after the previous state is left, but before the next state is entered
+signal state_change(state_from, state_to)
 
 onready var host = get_parent()
 var START_STATE = null
-var active = null
-var states = {}
 
+var active = null setget ,get_active_state
+var states = {}
 var state_data = {}
 
 func _ready():
   for state in get_children():
-    states[state.ID] = state
-    state_data[state.ID] = {}
+    if state.has_method('get_state_id'):
+      states[state.ID] = state
+      state_data[state.ID] = {}
 
-func start( start_state ):
-  if not states.has( start_state ):
+"""
+=== CORE METHODS
+"""
+
+func start(start_state_id):
+  if start_state_id in states:
+    START_STATE = start_state_id
+    active = states[start_state_id]
+    active.enter(self, state_data[START_STATE])
+
+func enter(state_to):
+  """
+  leave the currently active state, and enter a given State node
+  if we're already in that state, a state_change signal is not emitted
+  """
+  if not states.has(state_to):
     return null
 
-  START_STATE = start_state
-  active = states[start_state]
-  active.enter( self, null, get_state_data( START_STATE ) )
-
-# enter a given State node
-func enter( state_to ):
-  if not states.has( state_to ):
-    return null
-
-  # first, handle our current states
+  # first, handle our active State
   var state_from = get_active_id()
   if state_from:
     # if we're already in this state...
     if state_from == state_to:
-      # tell it to enter itself but don't modify the stack
-      return states[state_from].enter( self, null, get_state_data( state_from ) )
+      # tell it to enter itself from itself, but don't emit a state change
+      return states[state_to].enter(self, get_state_data(state_from), state_from)
     else:
       # otherwise, tell it to leave (deactivate)
-      get_state( state_from ).leave( self )
+      get_state(state_from).leave(self)
 
   # then, switch over to the next state
-  emit_signal( 'state_change', state_from, state_to )
-  active = states[state_to]
-  return get_active().enter( self, state_from, get_state_data( state_to ) )
+  emit_signal('state_change', state_from, state_to)
+  new_state = states[state_to]
+  active = new_state
+  return new_state.enter(self, get_state_data(state_to), state_from)
 
-# handle input
-func _input( ev ):
-  if get_active():
-    var next_state = get_active()._parse_input( self, ev )
+"""
+=== STATE WRAPPERS
+    These methods are here to wrap typical game loop signals such that only the currently
+    active state will handle them
+"""
+
+func _input(ev):
+  """
+  handle input based on the currently active state
+  """
+  if get_active_state():
+    var next_state = get_active_state()._parse_input(self, ev)
     if next_state:
-      enter( next_state )
+      enter(next_state)
 
-func _unhandled_input( ev ):
-  if get_active():
-    var next_state = get_active()._parse_unhandled_input( self, ev )
+func _unhandled_input(ev):
+  """
+  handle input based on the currently active state
+  """
+  if get_active_state():
+    var next_state = get_active_state()._parse_unhandled_input(self, ev)
     if next_state:
-      enter( next_state )
+      enter(next_state)
 
-func _process( delta ):
-  if get_active():
-    var next_state = get_active()._update( self, delta )
+func _process(delta):
+  """
+  handle game loop based on currently active state
+  """
+  if get_active_state():
+    var next_state = get_active_state()._update(self, delta)
     if next_state:
-      enter( next_state )
+      enter(next_state)
 
-func _physics_process( delta ):
-  if get_active():
-    var next_state = get_active()._physics_update( self, delta )
+func _physics_process(delta):
+  """
+  handle game physics loop based on currently active state
+  """
+  if get_active_state():
+    var next_state = get_active_state()._physics_update(self, delta)
     if next_state:
-      enter( next_state )
+      enter(next_state)
 
-func get_active():
+"""
+=== GETTERS
+"""
+
+func get_active_state():
   return active
 
 func get_active_id():
   if active:
-    return active.get_id()
+    return get_active_state().ID
   return null
 
-func get_state( id ):
-  if states.has( id ):
+func get_state(id):
+  if id in states:
     return states[id]
   return null
 
-func get_state_data( id ):
-  if state_data.has( id ):
+func get_state_data(id):
+  if id in state_data:
     return state_data[id]
-
   return null
 
-func set_state_data( id, data ):
-  if state_data.has( id ):
+func set_state_data(id, data):
+  if id in state_data:
     state_data[id] = data
-    return true
-
-  return false
+  return id in state_data
