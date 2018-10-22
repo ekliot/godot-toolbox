81c2119f858adf71ed2c8ecad00e468eb118b2d8
ekliot <ekliot@gmail.com>
2018-10-21 23:46:51 -0400

    logger source naming, timestamp

--- logger/logger.gd
+++ logger/logger.gd
@@ -6,7 +6,7 @@ const LVL_PAD = 5
 
 const FMT_PREFIX = "%*s"
 const FMT_LEVEL = "[%s]"
-const FMT_MSG = "%16s %8s // %s"
+const FMT_MSG = "%16s <%8s> %8s // %s"
 
 # TODO set this somewhere/somehow
 # TODO interpret these somewhere/somehow
@@ -35,16 +35,17 @@ func error(src, msg):
 func _log(src, lvl, msg):
   if lvl < logging_lvl:
     return
+  src = "%s/%s" % [src.get_parent().name, src.name]
   # TODO check for blocking
   print(_format_msg(src, lvl, msg))
   last_src = src
 
 func _format_msg(src, lvl, msg):
-  var pad = src.name.length() if src == last_src else 0
-  var who = src.name if src != last_src else ''
+  var pad = src.length() if src == last_src else 0
+  var who = src if src != last_src else ''
 
   var prefix = FMT_PREFIX % [pad, who]
   var level = FMT_LEVEL % LVL_STR[lvl]
-  var output = FMT_MSG % [prefix, level, msg]
+  var output = FMT_MSG % [prefix, OS.get_ticks_msec(), level, msg]
 
   return output
